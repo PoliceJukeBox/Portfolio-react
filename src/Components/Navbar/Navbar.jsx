c7b83c9295b0ad2bb40ffcb32fc2c861bde4c92c
Steau Vlad <vlad.steau11@gmail.com>
2024-07-11 02:38:00 +0300

    Navbar before adding icons

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,33 @@ function Navbar(){
 
     return(
         <>
-        <section id="navbar">Navbar Component</section>
+        <header className='header'>
+            <div className="logoDiv">
+                <h1 className="logo"><a href="#home">Sv.</a></h1>
+            </div>
+
+            <div className="navBar">
+                <ul className="navList">
+                    <li className="navItem">
+                        <a href="#about">1.<span>About</span></a>
+                    </li>
+                    <li className="navItem">
+                        <a href="#skills">2.<span>Skills</span></a>
+                    </li>
+                    <li className="navItem">
+                        <a href="#projects">3.<span>Projects</span></a>
+                    </li>
+                    <li className="navItem">
+                        <a href="#contact">4.<span>Contact</span></a>
+                    </li>
+                    <button className="btn"><a href="">Resume</a></button>
+                </ul>
+
+                <div className="closeNavBar">
+                    
+                </div>
+            </div>
+        </header>
         </>
     )
 }
