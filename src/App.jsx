2de6c872699b4dc321ceb2b515cc5d57053d0b24
Steau Vlad <vlad.steau11@gmail.com>
2024-07-11 00:42:28 +0300

    Done with main styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './Components/Home Section/Home'
 import Skills from './Components/Skills Section/Skills'
 import Project from './Components/Projects Section/Project'
 import Footer from './Components/Footer/Footer'
+import Contact from './Components/Contact Section/Contact'
 
 function App() {
 
