214f364bb717d979878d2a4f1d324853a28c2250
Steau Vlad <vlad.steau11@gmail.com>
2024-08-02 23:34:21 +0300

    Image css and text

--- src/Components/About Section/About.jsx
+++ src/Components/About Section/About.jsx
@@ -1,4 +1,7 @@
+import React from 'react';
 import './about.css'
+import { TbCloudDownload } from "react-icons/tb";
+import img from '../../assets/PozaSteauVlad.jpg';
 
 function About(){
 
@@ -7,7 +10,7 @@ function About(){
         <section id="about" className='about section container'>
             <div className="sectionTitle">
                 <span className="titleNumber">01.</span>
-                <h5 className="titleText">About Me
+                <h5 className="titleText">About Me  
                     <div className="underline"><span></span></div>
                 </h5>
             </div>
@@ -15,7 +18,7 @@ function About(){
             <div className="sectionContent grid">
                 <div className="textSection">
                     <h4>
-                    I'm a succsess-driven and determined developer. I have
+                    I'm a succses-driven and determined developer. I have
                     experience using HTML, Sass, Javascript and React to build
                     accessible component-based web pages and systems. <br />
                     Ability to deliver quality results using the multiple web 
@@ -25,6 +28,16 @@ function About(){
                     become a better developer than I was yesterday, determined and
                     result oriented.
                     </h4>
+
+                    <div className="aboutBtn">
+                        <a href="SteauVladCV.pdf" download="SteauVladCV.pdf" className="flex">
+                            Download CV <TbCloudDownload className='icon'/>
+                        </a>
+                    </div>
+                </div>
+
+                <div className="aboutImgDiv">
+                    <img src={img} alt="Image Steau Vlad" className='aboutImg'/>
                 </div>
             </div>
         </section>
