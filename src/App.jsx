94a4b3b88f51b56c756cc3efc8f5974565513b24
Steau Vlad <vlad.steau11@gmail.com>
2024-08-03 11:51:53 +0300

    Working on skill container

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App() {
       <Navbar/>
       <Home/>
       <About/>
-      {/* <Skills/>
-      <Project/>
+      <Skills/>
+      {/* <Project/>
       <Contact/>
       <Footer/> */}
     </>
--- src/Components/Skills Section/Skills.jsx
+++ src/Components/Skills Section/Skills.jsx
@@ -1,10 +1,74 @@
 import './skill.css'
+import { TbBrandReactNative } from "react-icons/tb";
 
 function Skills(){
 
     return(
         <>
-        <section id="skill">Skills Component</section>
+        <section id="skill" className='skills container section'>
+            <div className="sectionTitle">
+                <span className="titleNumber">02.</span>
+                <h5 className="titleText">Skills 
+                    <div className="underline"><span></span></div>
+                </h5>
+            </div>
+
+            <div className="skillsContainer grid">
+                <div className="skillGroup">
+                    <div className="groupTitle">
+                        <h2 className='title'>Web Development</h2>
+                        <span className="subTitle">2 years experience</span>
+                    </div>
+
+                    <div className="generalSkills">
+
+                        <div className="singleSkill">
+                            <div className="iconBox">
+                                <TbBrandReactNative className='icon'/>
+                            </div>
+                            <span className="skillName">React</span>
+                        </div>
+                        
+                        <div className="singleSkill">
+                            <div className="iconBox">
+                                <TbBrandReactNative className='icon'/>
+                            </div>
+                            <span className="skillName">React</span>
+                        </div>
+
+                        <div className="singleSkill">
+                            <div className="iconBox">
+                                <TbBrandReactNative className='icon'/>
+                            </div>
+                            <span className="skillName">React</span>
+                        </div>
+
+                        <div className="singleSkill">
+                            <div className="iconBox">
+                                <TbBrandReactNative className='icon'/>
+                            </div>
+                            <span className="skillName">React</span>
+                        </div>
+
+                        <div className="singleSkill">
+                            <div className="iconBox">
+                                <TbBrandReactNative className='icon'/>
+                            </div>
+                            <span className="skillName">React</span>
+                        </div>
+
+                        <div className="singleSkill">
+                            <div className="iconBox">
+                                <TbBrandReactNative className='icon'/>
+                            </div>
+                            <span className="skillName">React</span>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </section>
         </>
     )
 }
