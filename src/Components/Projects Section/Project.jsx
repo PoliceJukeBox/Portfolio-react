e86034294e4f43d1f115b36b3694c45af0adb51c
Steau Vlad <vlad.steau11@gmail.com>
2024-08-06 18:57:16 +0300

    Project card jsx

--- src/Components/Projects Section/Project.jsx
+++ src/Components/Projects Section/Project.jsx
@@ -1,4 +1,5 @@
 import './projects.css'
+import { FiGithub } from "react-icons/fi";
 
 function Project(){
 
@@ -13,7 +14,45 @@ function Project(){
             </div>
 
             <div className="projectContainer grid">
-                
+                {
+                    data.map(({id, github, image, liveLink, desc, demo, title, tech1, tech2, tech3, tech4}) =>{
+                        return(
+                            <div key={id} className="singleProject">
+                                <div className="externalLinks flex">
+                                    <div className="goToPageLink">
+                                        <a href={demo} target='_blank'>Go to site</a>
+                                    </div>
+                                    <div className="githubIcon">
+                                        <a href={github} target='_blank'>
+                                            <FiGithub className='icon'/>
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <div className="imgDiv">
+                                    <a href="" target='_blank'>
+                                        <img src={image} alt={title} />
+                                    </a>
+                                </div>
+
+                                <div className="projectTitle">
+                                    <h3>{title}</h3>
+                                </div>
+
+                                <div className="desc">
+                                    {desc}
+                                </div>
+
+                                <div className="technologies flex">
+                                    <small>{tech1}</small>
+                                    <small>{tech2}</small>
+                                    <small>{tech3}</small>
+                                    <small>{tech4}</small>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </section>
         </>
