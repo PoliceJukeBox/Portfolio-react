f3401695abc1d62a40db2e73ff51c14eca0efd45
Steau Vlad <vlad.steau11@gmail.com>
2024-08-07 14:07:21 +0300

    app.jsx cange name about

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import About from './Components/About Section/About'
+import About from './Components/AboutSection/About'
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Components/Home Section/Home'
 import Skills from './Components/Skills Section/Skills'
