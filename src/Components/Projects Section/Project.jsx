087d7ccf1ebce4e0bb85150d17d7136b7daa09ab
Steau Vlad <vlad.steau11@gmail.com>
2024-08-16 17:37:06 +0300

    Added button filters at project section

--- src/Components/Projects Section/Project.jsx
+++ src/Components/Projects Section/Project.jsx
@@ -1,6 +1,7 @@
 import './projects.css'
 import { FiGithub } from "react-icons/fi";
 import pozasv from '../../assets/pozasv.jpg'
+import { useState, useEffect } from 'react';
 
 const data = [
     {
@@ -8,8 +9,8 @@ const data = [
         image: pozasv,
         demo: 'https://portfolio-sv-eight.vercel.app/',
         github: '',
-        title: "Incercare",
-        dificulty: "hard",
+        title: "Incercare1",
+        category: "hard",
         desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatibus architecto soluta. Voluptatem, consequuntur quos! Aspernatur voluptatibus assumenda est doloremque, enim numquam at deserunt minus officiis sed delectus deleniti debitis!",
         tech1: 'react',
         tech2: 'scss',
@@ -21,8 +22,8 @@ const data = [
         image: pozasv,
         demo: 'https://portfolio-sv-eight.vercel.app/',
         github: '',
-        title: "Incercare",
-        dificulty: "hard",
+        title: "Incercare2",
+        category: "medium",
         desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatibus architecto soluta. Voluptatem, consequuntur quos! Aspernatur voluptatibus assumenda est doloremque, enim numquam at deserunt minus officiis sed delectus deleniti debitis!",
         tech1: 'react',
         tech2: 'scss',
@@ -34,8 +35,8 @@ const data = [
         image: pozasv,
         demo: 'https://portfolio-sv-eight.vercel.app/',
         github: '',
-        title: "Incercare",
-        dificulty: "hard",
+        title: "Incercare3",
+        category: "easy",
         desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatibus architecto soluta. Voluptatem, consequuntur quos! Aspernatur voluptatibus assumenda est doloremque, enim numquam at deserunt minus officiis sed delectus deleniti debitis!",
         tech1: 'react',
         tech2: 'scss',
@@ -47,8 +48,8 @@ const data = [
         image: pozasv,
         demo: 'https://portfolio-sv-eight.vercel.app/',
         github: '',
-        title: "Incercare",
-        dificulty: "hard",
+        title: "Incercare4",
+        category: "hard",
         desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatibus architecto soluta. Voluptatem, consequuntur quos! Aspernatur voluptatibus assumenda est doloremque, enim numquam at deserunt minus officiis sed delectus deleniti debitis!",
         tech1: 'react',
         tech2: 'scss',
@@ -58,6 +59,36 @@ const data = [
 ]
 
 function Project(){
+
+    const [selectedFilters, setSelectedFilters] = useState([]);
+    const [filteredItems, setFilteredItems] = useState(data);
+    let filters = ["easy", "medium", "hard"];
+
+    const handleFilterButtonClick = (selectedCategory) => {
+        console.log(selectedCategory);
+        if(selectedFilters.includes(selectedCategory)){
+           let filters = selectedFilters.filter((el) => el !== selectedCategory);
+           setSelectedFilters(filters);
+        }else{
+            setSelectedFilters([...selectedFilters, selectedCategory]);
+        }
+    };
+
+    useEffect(() =>{
+        filterItems();
+    }, [selectedFilters]);
+
+    const filterItems = () =>{
+        if(selectedFilters.length > 0){
+            let tempItems = selectedFilters.map((selectedCategory) =>{
+                let temp = data.filter((item) => item.category ===selectedCategory)
+                return temp;
+            });
+            setFilteredItems(tempItems.flat());
+        }else{
+            setFilteredItems([...data]);
+        }
+    };
 
     return(
         <>
@@ -69,17 +100,29 @@ function Project(){
                 </h5>
             </div>
 
+            <div className="dificultyFilter flex">
+                {filters.map((category, idx) => (
+                    <button
+                        onClick={() => handleFilterButtonClick(category)}
+                        className={`button ${selectedFilters?.includes(category) ? "active": ""}`}
+                        key={`filters-${idx}`}
+                    >
+                        {category}
+                    </button>
+                ))}
+            </div>
+
             <div className="projectContainer grid">
                 {
-                    data.map(({id, github, image, desc, demo, title, tech1, tech2, tech3, tech4}) =>{
+                    filteredItems.map((item, idx) =>{
                         return(
-                            <div key={id} className="singleProject">
+                            <div key={`data-${idx}`} className="singleProject">
                                 <div className="externalLinks flex">
                                     <div className="goToPageLink">
-                                        <a href={demo} target='_blank'>Go to site</a>
+                                        <a href={item.demo} target='_blank'>Go to site</a>
                                     </div>
                                     <div className="githubIcon">
-                                        <a href={github} target='_blank'>
+                                        <a href={item.github} target='_blank'>
                                             <FiGithub className='icon'/>
                                         </a>
                                     </div>
@@ -87,23 +130,23 @@ function Project(){
 
                                 <div className="imgDiv">
                                     <a href="" target='_blank'>
-                                        <img src={image} alt={title} />
+                                        <img src={item.image} alt={item.title} />
                                     </a>
                                 </div>
 
                                 <div className="projectTitle">
-                                    <h3>{title}</h3>
+                                    <h3>{item.title}<span> - </span><span className={item.category}>{item.category}</span></h3>
                                 </div>
 
                                 <div className="desc">
-                                    {desc}
+                                    {item.desc}
                                 </div>
 
                                 <div className="technologies flex">
-                                    <small>{tech1}</small>
-                                    <small>{tech2}</small>
-                                    <small>{tech3}</small>
-                                    <small>{tech4}</small>
+                                    <small>{item.tech1}</small>
+                                    <small>{item.tech2}</small>
+                                    <small>{item.tech3}</small>
+                                    <small>{item.tech4}</small>
                                 </div>
                             </div>
                         )
@@ -116,3 +159,5 @@ function Project(){
 }
 
 export default Project;
+
+//{id, github, image, desc, demo, title, category, tech1, tech2, tech3, tech4}
