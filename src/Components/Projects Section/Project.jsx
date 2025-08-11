0276bf48d8e9b91475f949bd2b41a34ab3266a4f
Steau Vlad <vlad.steau11@gmail.com>
2025-08-11 22:12:22 +0300

    Removed Supabase from the project

--- src/Components/Projects Section/Project.jsx
+++ src/Components/Projects Section/Project.jsx
@@ -1,219 +1,180 @@
+import {useState, useEffect} from 'react';
+
 import './projects.css'
 import { FiGithub } from "react-icons/fi";
-import { useState, useEffect } from 'react';
 
-//-----------------------------SupaBase-----------------------------------
+// Example local data
+import calculatormedie from './../../assets/calculatorMedie.jpg'; // <-- adjust paths
+import weatherapp from './../../assets/weatherApp.jpg';
+import pozasv from './../../assets/pozasv.jpg';
 
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://dezyzjeqaghhsvbzaajt.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlenl6amVxYWdoaHN2YnphYWp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQwNTAwMDYsImV4cCI6MjAzOTYyNjAwNn0.XhdHX0xo_yDKhevenW88POZWI7VuxnoUVBQdyC99pMA'
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-console.log(supabase);
-
-// const data = [
-//     {
-//         id: 1,
-//         image: calculatormedie,
-//         demo: 'https://portfolio-sv-eight.vercel.app/',
-//         github: '',
-//         title: "Incercare1",
-//         category: "hard",
-//         desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatibus architecto soluta. Voluptatem, consequuntur quos! Aspernatur voluptatibus assumenda est doloremque, enim numquam at deserunt minus officiis sed delectus deleniti debitis!",
-//         features: "daaaaaaaaaaaaaaaaaaa",
-//         skills: "maaaaaaaaaaaa",
-//         tech1: 'react',
-//         tech2: 'scss',
-//         tech3: 'mysql(supabase)',
-//         tech4: '' 
-//     },
-//     {
-//         id: 2,
-//         image: weatherapp,
-//         demo: 'https://portfolio-sv-eight.vercel.app/',
-//         github: '',
-//         title: "Incercare2",
-//         category: "medium",
-//         desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatibus architecto soluta. Voluptatem, consequuntur quos! Aspernatur voluptatibus assumenda est doloremque, enim numquam at deserunt minus officiis sed delectus deleniti debitis!",
-//         features: "daaaaaaaaaaaaaaaaaaa",
-//         skills: "maaaaaaaaaaa",
-//         tech1: 'react',
-//         tech2: 'scss',
-//         tech3: 'mysql(supabase)',
-//         tech4: '' 
-//     },
-//     {
-//         id: 3,
-//         image: pozasv,
-//         demo: 'https://portfolio-sv-eight.vercel.app/',
-//         github: '',
-//         title: "Incercare3",
-//         category: "easy",
-//         desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatibus architecto soluta. Voluptatem, consequuntur quos! Aspernatur voluptatibus assumenda est doloremque, enim numquam at deserunt minus officiis sed delectus deleniti debitis!",
-//         features: "daaaaaaaaaaaaaaaaaaa",
-//         skills: "maaaaaaaaaaa",
-//         tech1: 'react',
-//         tech2: 'scss',
-//         tech3: 'mysql(supabase)',
-//         tech4: 'aaa' 
-//     },
-//     {
-//         id: 4,
-//         image: pozasv,
-//         demo: 'https://portfolio-sv-eight.vercel.app/',
-//         github: '',
-//         title: "Incercare4",
-//         category: "hard",
-//         desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatibus architecto soluta. Voluptatem, consequuntur quos! Aspernatur voluptatibus assumenda est doloremque, enim numquam at deserunt minus officiis sed delectus deleniti debitis!",
-//         features: "daaaaaaaaaaaaaa",
-//         skills: "maaaaaaaaaaaaaa",
-//         tech1: 'react',
-//         tech2: 'scss',
-//         tech3: 'mysql(supabase)',
-//         tech4: '' 
-//     }
-// ]
-
-function Project(){
-
-    const [fetchError, setFetchError] = useState(null);
-    const [smoothies, setSmoothies] = useState(null);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() =>{
-        setLoading(false)
-        const fetchSmoothies = async () => {
-            
-            const { data, error } = await supabase.from('data').select('*')
-            if(error) {
-                setFetchError('Cound not fetch');
-                setSmoothies(null);
-                console.log(error);
-            }
-            setLoading(true);
-            if(data) {
-                setSmoothies(data);
-                setFetchError(null);
-            }
-        }
-        fetchSmoothies();
-    }, [])
-
-    
-    console.log(loading);
-
-    if(!loading){
-        return
+const data = [
+    {
+        id: 1,
+        image: calculatormedie,
+        demo: 'https://portfolio-sv-eight.vercel.app/',
+        github: '',
+        title: "Incercare1",
+        category: "hard",
+        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatibus architecto soluta...",
+        features: "daaaaaaaaaaaaaaaaaaa",
+        skills: "maaaaaaaaaaaa",
+        tech1: 'react',
+        tech2: 'scss',
+        tech3: 'mysql(supabase)',
+        tech4: '' 
+    },
+    {
+        id: 2,
+        image: weatherapp,
+        demo: 'https://portfolio-sv-eight.vercel.app/',
+        github: '',
+        title: "Incercare2",
+        category: "medium",
+        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatibus architecto soluta...",
+        features: "daaaaaaaaaaaaaaaaaaa",
+        skills: "maaaaaaaaaaa",
+        tech1: 'react',
+        tech2: 'scss',
+        tech3: 'mysql(supabase)',
+        tech4: '' 
+    },
+    {
+        id: 3,
+        image: pozasv,
+        demo: 'https://portfolio-sv-eight.vercel.app/',
+        github: '',
+        title: "Incercare3",
+        category: "easy",
+        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatibus architecto soluta...",
+        features: "daaaaaaaaaaaaaaaaaaa",
+        skills: "maaaaaaaaaaa",
+        tech1: 'react',
+        tech2: 'scss',
+        tech3: 'mysql(supabase)',
+        tech4: 'aaa' 
+    },
+    {
+        id: 4,
+        image: pozasv,
+        demo: 'https://portfolio-sv-eight.vercel.app/',
+        github: '',
+        title: "Incercare4",
+        category: "hard",
+        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptatibus architecto soluta...",
+        features: "daaaaaaaaaaaaaa",
+        skills: "maaaaaaaaaaaaaa",
+        tech1: 'react',
+        tech2: 'scss',
+        tech3: 'mysql(supabase)',
+        tech4: '' 
     }
-    // const [selectedFilters, setSelectedFilters] = useState([]);
-    // const [filteredItems, setFilteredItems] = useState(data);
-    // let filters = ["easy", "medium", "hard"];
+];
 
-    // const handleFilterButtonClick = (selectedCategory) => {
-    //     console.log(selectedCategory);
-    //     if(selectedFilters.includes(selectedCategory)){
-    //        let filters = selectedFilters.filter((el) => el !== selectedCategory);
-    //        setSelectedFilters(filters);
-    //     }else{
-    //         setSelectedFilters([...selectedFilters, selectedCategory]);
-    //     }
-    // };
+function Project() {
+    const [selectedFilters, setSelectedFilters] = useState([]);
+    const [filteredItems, setFilteredItems] = useState(data);
+    const filters = ["easy", "medium", "hard"];
 
-    // useEffect(() =>{
-    //     filterItems();
-    // }, [selectedFilters]);
+    const handleFilterButtonClick = (selectedCategory) => {
+        if (selectedFilters.includes(selectedCategory)) {
+            // Remove category if already selected
+            let updatedFilters = selectedFilters.filter((el) => el !== selectedCategory);
+            setSelectedFilters(updatedFilters);
+        } else {
+            // Add new category
+            setSelectedFilters([...selectedFilters, selectedCategory]);
+        }
+    };
 
-    // const filterItems = () =>{
-    //     if(selectedFilters.length > 0){
-    //         let tempItems = selectedFilters.toReversed().map((selectedCategory) =>{
-    //             let temp = smoothies.filter((item) => item.category ===selectedCategory)
-    //             return temp;
-    //         });
-    //         setFilteredItems(tempItems.flat());
-    //     }else{
-    //         setFilteredItems([...smoothies]);
-    //     }
-    // };
+    useEffect(() => {
+        if (selectedFilters.length > 0) {
+            let tempItems = selectedFilters
+                .toReversed()
+                .map((selectedCategory) => {
+                    return data.filter((item) => item.category === selectedCategory);
+                });
+            setFilteredItems(tempItems.flat());
+        } else {
+            setFilteredItems([...data]);
+        }
+    }, [selectedFilters]);
 
-    // console.log(filteredItems)
-
-    return(
-        <>
+    return (
         <section id="projects" className='projects container section'>
             <div className="sectionTitle">
                 <span className="titleNumber">03.</span>
-                <h5 className="titleText">Projects 
+                <h5 className="titleText">
+                    Projects
                     <div className="underline"><span></span></div>
                 </h5>
             </div>
 
-            {/* <div className="dificultyFilter flex">
+            {/* Category Filter Buttons */}
+            <div className="dificultyFilter flex">
                 {filters.map((category, idx) => (
                     <button
                         onClick={() => handleFilterButtonClick(category)}
-                        className={`button ${selectedFilters?.includes(category) ? "active": ""}`}
+                        className={`button ${selectedFilters.includes(category) ? "active" : ""}`}
                         key={`filters-${idx}`}
                     >
                         {category}
                     </button>
                 ))}
-            </div> */}
+            </div>
 
+            {/* Projects Grid */}
             <div className="projectContainer grid">
-                {
-                    // filteredItems.toReversed()
-                    smoothies.map((item, idx) =>{ 
-                        return(
-                            <div key={`data-${idx}`} className="singleProject">
-                                <div className="externalLinks flex">
-                                    <div className="goToPageLink">                                                                        
-                                         <a className={item.demo} href={item.demo} target='_blank'>Go to site</a>                   
-                                    </div>
-                                    <div className="githubIcon">
-                                        <a href={item.github} target='_blank'>
-                                            <FiGithub className='icon'/>
-                                        </a>
-                                    </div>
-                                </div>
-
-                                <div className="imgDiv">
-                                    <a href="" target='_blank'>
-                                        <img src={item.image} alt={item.title} />
-                                    </a>
-                                </div>
-
-                                <div className="projectTitle">
-                                    <h3>{item.title}<span> - </span><span className={item.category}>{item.category}</span></h3>
-                                </div>
-
-                                <div className="desc">
-                                    {item.desc}
-                                </div>
-
-                                <div className="features">
-                                    <b>Features:</b> {item.features}
-                                </div>
-
-                                <div className="skills">
-                                    <b>Skills:</b> {item.skills}
-                                </div>
-
-                                <div className="technologies flex">
-                                    <small>{item.tech1}</small>
-                                    <small>{item.tech2}</small>
-                                    <small>{item.tech3}</small>
-                                    <small>{item.tech4}</small>
-                                </div>
+                {filteredItems.toReversed().map((item, idx) => (
+                    <div key={`data-${idx}`} className="singleProject">
+                        <div className="externalLinks flex">
+                            <div className="goToPageLink">
+                                <a href={item.demo} target='_blank' rel="noopener noreferrer">
+                                    Go to site
+                                </a>
                             </div>
-                        )
-                    })
-                }
+                            <div className="githubIcon">
+                                <a href={item.github} target='_blank' rel="noopener noreferrer">
+                                    <FiGithub className='icon' />
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="imgDiv">
+                            <a href={item.demo} target='_blank' rel="noopener noreferrer">
+                                <img src={item.image} alt={item.title} />
+                            </a>
+                        </div>
+
+                        <div className="projectTitle">
+                            <h3>
+                                {item.title}
+                                <span> - </span>
+                                <span className={item.category}>{item.category}</span>
+                            </h3>
+                        </div>
+
+                        <div className="desc">{item.desc}</div>
+
+                        <div className="features">
+                            <b>Features:</b> {item.features}
+                        </div>
+
+                        <div className="skills">
+                            <b>Skills:</b> {item.skills}
+                        </div>
+
+                        <div className="technologies flex">
+                            <small>{item.tech1}</small>
+                            <small>{item.tech2}</small>
+                            <small>{item.tech3}</small>
+                            <small>{item.tech4}</small>
+                        </div>
+                    </div>
+                ))}
             </div>
         </section>
-        </>
-    )
+    );
 }
 
 export default Project;
-
-//{id, github, image, desc, demo, title, category, tech1, tech2, tech3, tech4}
